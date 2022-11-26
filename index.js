d481d1c303b5b9c8326df54573e389790716fa68
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2022-11-26 19:19:25 +0600

    database connected

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 require('dotenv').config();
 
@@ -9,6 +10,14 @@ require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3aa5vlu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 
 app.get('/', (req, res)=>{
     res.send('AK-Hands Fashion Shop Running')
