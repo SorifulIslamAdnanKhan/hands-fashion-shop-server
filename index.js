728cf018a3ec26f5a6b4ca66c950145e2c95fb8c
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2022-11-27 23:17:43 +0600

    user post api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, MongoRuntimeError } = require('mongodb');
 
 require('dotenv').config();
 
@@ -16,8 +16,34 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run(){
+    try{
 
+        const usersCollection = client.db("handsFashionShop").collection("users");
 
+        // Users API
+
+        app.post('/users', async (req, res)=>{
+            const user = req.body;
+            const exist = await usersCollection.findOne({email: user.email});
+            if(exist){
+                return res.send({status: 0, message: 'User already exist.'})
+            }
+            
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+        
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.log(err));
 
 app.get('/', (req, res)=>{
     res.send('AK-Hands Fashion Shop Running')
